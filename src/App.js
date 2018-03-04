c62ef7e779aeebdeef6e9d4c46a1ea286f31f50e
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-03-04 19:32:21 +0200

    feat: basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Auth from './containers/auth/Auth';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <Auth />
-      </div>
+      </Router>
     );
   }
 }
--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import LoginForm from './login-form/LoginForm';
 import SignUpForm from './signup-form/SignUpForm';
 
@@ -6,9 +7,14 @@ class Auth extends Component {
   render() {
     return (
       <div>
-        <LoginForm/>
-        <SignUpForm/>
-        this is auth
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
+        <div>
+          <Route path='/login' component={LoginForm}/>
+          <Route path='/signup' component={SignUpForm}/>
+        </div>
       </div>
     );
   }
